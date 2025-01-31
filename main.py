fc35b3bcc21264c2e87feaed96cfdbeb2e644c5f
Satoshi Watanabe <sassy.watanabe@gmail.com>
2025-01-31 09:53:37 +0900

    Webページが改変されていたので変更

--- main.py
+++ main.py
@@ -7,42 +7,41 @@ from bs4 import BeautifulSoup
 import json
 from datetime import date
 
-base_url = 'https://www.momoclo.net'
-news_url = base_url + '/news/'
-
-def get_date_for_momoclo_page(dt):
-    year_div = dt.find("div", {"class": "year"}).string
-    date_div = dt.find("div", {"class": "date"}).string
-    return year_div + "/" + date_div
+base_url = "https://www.momoclo.net"
+news_url = base_url + "/news/"
 
 
 def get_web_page(url):
-    req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-    return urllib.request.urlopen(req).read().decode('utf-8')
+    req = urllib.request.Request(url, headers={"User-Agent": "Mozilla/5.0"})
+    return urllib.request.urlopen(req).read().decode("utf-8")
+
 
 def parse(soup):
     news_dict = []
-    articles = soup.find_all("div", {"class": "article"})
+    articles = soup.find_all("div", {"class": "news_list_container"})
     for article in articles:
-        article_date = get_date_for_momoclo_page(article.find("dt"))
+        article_date = article.find("p", {"class": "news_list_date"}).string.replace(
+            ".", "/"
+        )
+        article_title = article.find("p", {"class": "news_list_text"}).string
+        article_url = article.find("a", {"class": "news_list_link"})["href"]
         news = {
-            "date" : article_date,
-            "title" : article.find("h4").string,
-            "url" : base_url + article.find("a")["href"]
+            "date": article_date,
+            "title": article_title,
+            "url": article_url,
         }
         news_dict.append(news)
     return news_dict
+
 
 def main():
     page = get_web_page(news_url)
     soup = BeautifulSoup(page, "html.parser")
     news = parse(soup)
-    output = {
-        "updated": date.today().strftime("%Y/%m/%d"),
-        "news": news
-    }
-    with open('data/news.json', mode="w") as f:
+    output = {"updated": date.today().strftime("%Y/%m/%d"), "news": news}
+    with open("data/news.json", mode="w") as f:
         json.dump(output, f, ensure_ascii=False, indent=4)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
